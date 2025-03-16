34fef03f5aa7381e9924c6874dfc3f8fc0f5a490
azkbbys <oyroyroyr@163.com>
2025-03-16 12:12:01 +0800

    feat(bysTools): 添加环境变量配置并优化程序路径设置
    - 新增环境变量 BYSTOOLS 用于存储程序安装路径
    - 用户可在首次运行时输入安装路径，自动写入环境变量
    - 程序启动时自动从环境变量读取安装路径
    - 优化了程序启动、更新和资源文件路径的处理
    - 删除了 config.txt 文件，改为使用环境变量存储路径信息

--- bysTools.py
+++ bysTools.py
@@ -7,19 +7,21 @@ import tkinter
 import functions.update
 from os import listdir
 from os.path import isdir
+import subprocess
+import os
 def exitnow():
     sleep(1)
     info('程序运行结束')
     exit()
 def program(name:str):
-    add_ons = listdir('./add-ons')
+    add_ons = listdir(path+'add-ons')
     # info('查找到程序'+str(add_ons))
-    add_ons = [i for i in add_ons if isdir('./add-ons/'+i)]
+    add_ons = [i for i in add_ons if isdir(path+'add-ons/'+i)]
     # 判断是否有这个程序
     if name in add_ons:
         # 执行这个程序
         info('运行程序'+name)
-        system('start ./add-ons/'+name+'/main.exe')
+        system('start '+path+'add-ons/'+name+'/main.exe')
     else:
         warn('找不到程序'+name+'，正在引导下载')
         not tkinter.messagebox.askyesno('下载附加程序', '要运行此功能，您需要下载附加包'+name+'，点击“是”将打开下载页面') or system('start https://github.com/azkbbys/'+name+'/releases')
@@ -32,6 +34,11 @@ try:
         pass
 except:
     error('配置文件不存在，正在创建')
+    info('请在下方输入本程序安装路径，以你的盘符开始，以“bysTools\\”结尾')
+    path = input()
+    # 用户输入路径并写入系统环境变量
+    subprocess.run(['setx', 'BYSTOOLS', path, '/M'], shell=True)
+    info(f'已将路径 {path} 写入系统环境变量 BYSTOOLS')
     with open('config.txt','w') as f:
         f.write('version: {v}\n'.format(v=functions.info.version))
     info('配置文件创建成功！请尝试重新运行程序！')
@@ -39,6 +46,8 @@ except:
 with open('config.txt') as f:
     config = f.readlines()
 info('配置文件读取成功')
+path = os.getenv('BYSTOOLS').replace('\\','/')
+info('已获取环境变量BYSTOOLS:'+str(path))
 info('正在检查更新')
 if(functions.update.check()=='已是最新版本'):
     info('已是最新版本'+functions.info.version)
@@ -53,7 +62,7 @@ warn('请不要关闭本界面')
 root = tkinter.Tk()
 root.title('bysTools-v'+functions.info.version)
 root.geometry('500x500')
-root.iconbitmap('./images/icon.ico')
+root.iconbitmap(path+'images\icon.ico')
 root.resizable(False, False)
 lable = tkinter.Label(root, text='bysTools\n请点击下方按钮选择功能', font=('微软雅黑', 15))
 lable.pack()
